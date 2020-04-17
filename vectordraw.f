31ca93457709445738db7e70c5f8367d9b3660bd
harrypower <philipkingsmith@gmail.com>
2020-04-17 13:26:25 -0600

    changed date

--- vectordraw.f
+++ vectordraw.f
@@ -1,6 +1,6 @@
 \ vectordraw.f
 
-\    Copyright (C) 2019  Philip King Smith
+\    Copyright (C) 2020  Philip King Smith
 
 \    This program is free software: you can redistribute it and/or modify
 \    it under the terms of the GNU General Public License as published by
