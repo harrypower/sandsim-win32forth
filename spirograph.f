a29e84d515855914f164df115c053b8181fef012
harrypower <philipkingsmith@gmail.com>
2020-06-17 14:12:22 -0600

    fixed polar>rect calculation and adjusted threeleggedspirl

--- spirograph.f
+++ spirograph.f
@@ -53,31 +53,34 @@ gforthtest true = [if]
 0 value bangle3
 0 value distance3
 0 value nsteps
+0 value xend
+0 value yend
 
 : polar>rect ( nangle ndistance - nx ny ) \ convert angle distance to x and y corrodinates with unit circle math
   2dup
-  s>f swap deg>rads fcos f* f>s \ nx
-  s>f swap deg>rads fsin f* f>s ; \ ny 
+  s>f deg>rads fcos f* \ ( nangle ndistance -- ) ( -- :r nx )
+  s>f deg>rads fsin f* f>s f>s swap ; \ ( nx ny -- )
 
-: threeleggedspiral ( ntimes angle1 distance1 angle2 distance2 angel3 distance3 -- ) \ draws 3 legged spirograph assuming current x and y possion is start of pattern and 0 angle is start for all 3 legs
+: threeleggedspiral ( ntimes nx ny angle1 distance1 angle2 distance2 angel3 distance3 -- ) \ draws 3 legged spirograph assuming current x and y possion is start of pattern and 0 angle is start for all 3 legs
   to distance3
   to aangle3
   to distance2
   to aangle2
   to distance1
   to aangle1
+  to yend
+  to xend
   to nsteps
-  xposition to x3
-  yposition to y3
   0 to bangle1
   0 to bangle2
   0 to bangle3
-  180 distance3 polar>rect y3 + to y2 x3 + to x2
+  180 distance3 polar>rect yend + to y2 xend + to x2
   180 distance2 polar>rect y2 + to y1 x2 + to x1
   180 distance1 polar>rect y1 + to ycenter x1 + to xcenter \ now i have the center coordinates
   nsteps 0 ?do
-  aangle1 bangle1 + dup to bangle1 distance1 polar>rect ycenter + to y1 xcenter + to x1
-  aangle2 bangle2 + dup to bangle2 distance2 polar>rect y1 + to y2 x1 + to x2
-  aangle3 bangle3 + dup to bangle3 distance3 polar>rect y2 + to y3 x2 + to x3 \ now i have new x an y coordinates to move to
-  xposition yposition x3 y3 drawline . \ now xposition and yposition have moved so redo this caluclation from center again
+    aangle1 bangle1 + dup to bangle1 distance1 polar>rect ycenter + to y1 xcenter + to x1
+    aangle2 bangle2 + dup to bangle2 distance2 polar>rect y1 + to y2 x1 + to x2
+    aangle3 bangle3 + dup to bangle3 distance3 polar>rect y2 + to y3 x2 + to x3 \ now i have new x an y coordinates to move to
+    xend yend x3 y3 drawline . \ now xposition and yposition have moved so redo this caluclation from center again
+    x3 to xend y3 to yend
   loop ;
