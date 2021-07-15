45ae7530f391a1fb8960db31e994e7921a15591f
harrypower <philipkingsmith@gmail.com>
2021-07-15 00:19:45 -0600

    started vectortools.f xy raw conversion tool

---
+++ vectortools.f
@@ -0,0 +1,127 @@
+\ vectortools.f
+
+\    Copyright (C) 2020  Philip King Smith
+
+\    This program is free software: you can redistribute it and/or modify
+\    it under the terms of the GNU General Public License as published by
+\    the Free Software Foundation, either version 3 of the License, or
+\    (at your option) any later version.
+
+\    This program is distributed in the hope that it will be useful,
+\    but WITHOUT ANY WARRANTY; without even the implied warranty of
+\    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\    GNU General Public License for more details.
+
+\    You should have received a copy of the GNU General Public License
+\    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+\ tools to convert gcode to vector data used by vectordraw.f
+
+\ Requires:
+
+\ Revisions:
+\ 07/01/2021 started coding
+
+needs linklist.f
+needs extstruct.f
+
+10 set-precision
+0.0e fvalue tempx
+0.0e fvalue tempy
+0 value fid
+256 value buffersize
+0 value xypairsize
+buffersize chars buffer: xypair$
+\ 0.0e fvalue fx1
+\ 0.0e fvalue fy1
+\ 0.0e fvalue fx2
+\ 0.0e fvalue fy2
+\ 0.0e fvalue fangle
+\ 0.0e fvalue fdistance
+
+: openrawfile ( -- )
+  s" c:\Users\Philip\Documents\inkscape-stuff\raw.data" r/o open-file throw to fid ;
+
+: getxf ( naddr u -- nflag  fs: -- fx ) \ string naddr u if it contains the x string turn it in floating stack and true
+    2dup s"  " search if swap drop - >float if true else false 0.0e then else 2drop 2drop false 0.0e then ;
+
+: getyf ( naddr u -- nflag  fs: -- fy ) \ string naddr u if it contains the y string turn it in floating stack and true
+\ if string is not understandable as a floating number then return false and the floating stack contains 0.0e
+  s"  " search if 1 /string  -trailing >float if true else false 0.0e then else 2drop false 0.0e then ;
+
+: getxyf ( naddr u -- nflag fs: -- fx fy )
+  2dup getxf rot rot getyf and ;
+
+: getxypair ( -- nflag fs: -- fx fy )
+\ nflag is true if an xy pair was read in from file
+\ nflag is false if the file has no more lines to read or if the raw.data file is not readable
+\ the floating value of xy pair are returned on floating stack and is only valid if nflag is true
+  xypair$ buffersize fid read-line throw
+  if
+    xypair$ swap getxyf
+  else
+    drop false 0.0e 0.0e
+  then ;
+
+:struct rawpoint
+  b/float fx
+  b/float fy
+;struct
+
+:struct vectordata
+  b/float fangle
+  b/float fdistance
+;struct
+
+\ rawpoint vectordata  \ this is needed to allow the structure members to be visible after this point in code for compiling
+\ sizeof rawpoint allocate throw value myfloat
+\ f# 123.234503 myfloat fx f!
+\ previous previous \ these are needed to remove the visability of the structure members exposed above
+
+\ Define Object containing the line data list
+:OBJECT rawxy <SUPER Linked-List
+
+:M ClassInit:  ( -- ) \ constructor
+  ClassInit: super
+  ;M
+
+:M ~: ( -- ) \ destructor
+  \ first remove all the allocated floating data in the list here
+  >firstlink: self
+  #links: self 1 - 0 ?do
+    data@: self >nextlink: self
+    dup 0 = if drop else free throw then
+  loop
+  ~: super
+  ;M
+
+:M fxy!: ( -- f: fx fy -- ) \ store fx and fy in list
+  sizeof rawpoint allocate throw
+  [ rawpoint ]
+  dup dup fy f! fx f!
+  data!: self addlink: self
+  [ previous ]
+  ;M
+
+:M fxy@: ( -- f: -- fx fy ) \ retrieve next nx ny from list
+  data@: self >nextlink: self
+  [ rawpoint ]
+  dup fx f@ fy f@
+  [ previous ]
+  ;M
+
+:M qnt: ( -- nline-qnt ) \ return how many data pairs
+  #Links: self 1 - ;M
+
+;OBJECT
+
+\ previous previous
+
+: readrawxy ( -- ) \ opens and reads raw.data file and puts the xy data in rawxy linked list
+  openrawfile
+  qnt: rawxy 0 <> if ~: rawxy then
+  begin
+    getxypair
+    if fxy!: rawxy false else fdrop fdrop true then
+  until
+  fid close-file throw ;
